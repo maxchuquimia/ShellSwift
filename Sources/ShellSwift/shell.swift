362bddb06eaf6c07a356c4e8788b098632611ef3
Max Chuquimia <max.chuquimia@users.noreply.github.io>
2022-08-08 21:45:09 +1000

    waitUntilExit()

--- Sources/ShellSwift/shell.swift
+++ Sources/ShellSwift/shell.swift
@@ -10,20 +10,23 @@ import Foundation
 public func shell(_ command: String) throws -> String { try shell()(command) }
 
 public func shell() -> (String) throws -> String {{
-     let task = Process()
-       task.launchPath = "/bin/bash"
-       task.arguments = ["-c", $0]
+    let task = Process()
+    task.launchPath = "/bin/bash"
+    task.arguments = ["-c", $0]
 
-       let pipe = Pipe()
-       task.standardOutput = pipe
-       task.launch()
+    let pipe = Pipe()
+    task.standardOutput = pipe
 
-       let data = pipe.fileHandleForReading.readDataToEndOfFile()
-       let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
+    task.launch()
 
-       if task.terminationStatus != 0 {
-           throw "Task failed with code \(task.terminationStatus)"
-       }
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    task.waitUntilExit()
 
-       return output
+    let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
+
+    if task.terminationStatus != 0 {
+        throw "Task failed with code \(task.terminationStatus)"
+    }
+
+    return output
 }}
